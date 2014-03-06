0fba1994b1d3974019307aa45b446b66096d7a61
vicmagv <vicmagv@75631292-8e74-5e46-0caf-63767b05a9cc>
2014-03-06 09:17:14 +0000

    Adjunto avances en el caso base.

---
+++ trunk/PracticasEda2/src/practica01/Usuario.java
@@ -0,0 +1,43 @@
+package practica01;
+
+public class Usuario {
+	private int id;
+	private int ce;
+	private int ice;
+	
+	public Usuario(int id, int ce, int ice){
+		this.id=id;
+		this.ce=ce;
+		this.ice=ice;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCe() {
+		return ce;
+	}
+
+	public void setCe(int ce) {
+		this.ce = ce;
+	}
+
+	public int getIce() {
+		return ice;
+	}
+
+	public void setIce(int ice) {
+		this.ice = ice;
+	}
+	
+	public String toString(){
+		return "["+id+" "+ice+" "+ce+"]";
+		
+	}
+	
+}
